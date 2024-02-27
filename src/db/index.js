dd9466de62c001fe2ec84f59e768cf87962285c0
nayeemmehedi <boyneel79@gmail.com>
2024-02-28 01:58:23 +0600

    db name change

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,18 @@
 import mongoose from "mongoose";
 
+const url = process.env.db_url;
+
+async function dbConnection() {
+  try {
+    await mongoose.connect(url);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default dbConnection;
+
+// local host
 // const dbConnection = async () => {
 //   try {
 //     const mongooseConnection = await mongoose.connect(process.env.DB_NAME );
@@ -16,28 +29,3 @@ import mongoose from "mongoose";
 // export default dbConnection;
 
 // import { app } from "./app.js";
-
-
-
-import { MongoClient, ServerApiVersion } from 'mongodb';
-import { app } from "../app.js";
-const uri = "mongodb+srv://nayeemmehedi:nayeemmehedi41@cluster0.teacx.mongodb.net/muslim-sweets";
-
-
-
-async function dbConnection() {
-  try {
-    await mongoose.connect(uri)
-    // // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    // // app.listen(process.env.PORT ?? 4700);
-  }catch(error){
-    console.log(error)
-  }
-}
-
-export default dbConnection;
-
